9c68b012c135081a259daa0229db6f5656a1f85d
SyedSalahuddin1 <humbleakh21@gmail.com>
2026-01-24 23:29:27 +0530

    Another Project on the way

---
+++ cli-quiz-engine.py
@@ -0,0 +1,24 @@
+def get_questions():
+    questions = [
+        {
+            "Question": "Who is the first Prime Minister of India? ",
+            "Answers": ["Mahatma Ghandi", "V.P Patil", "Jawaharlal Nehru"],
+            "Correct": 3
+        },
+        {
+          "Question": "Who is the current Welterweight Champion of UFC:",
+          "Answers": ["Kumaru Usman", "JDM", "Islam"],
+          "Correct":3
+        },
+        {
+            "Question":"Who is considered the greatest boxer of all-time",
+            "Answers":["Mike Tyson", "Joe Frazier", "Mohammed Ali"],
+            "Correct":3
+        }
+    ]
+    
+    return questions
+
+def ask_questions():
+    
+    pass
