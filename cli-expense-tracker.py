ef8128bc35acf256a7cb50be67c64212e78e0e82
SyedSalahuddin1 <humbleakh21@gmail.com>
2026-01-27 18:35:44 +0530

    Phase 6.4 Completed

--- cli-expense-tracker.py
+++ cli-expense-tracker.py
@@ -1,46 +1,99 @@
-def get_int(prompt):
-    
-    while True:
-        try:
-            return int(input(prompt))
-        except ValueError:
-            print("Please enter only number")
-            
+class InputHelper:
+    def get_int(self, prompt):
+        
+        while True:
+            try:
+                return int(input(prompt))
+            except ValueError:
+                print("Please enter only number")
+                
 
-def get_float(prompt):
-    while True:
-        try:
-            return float(input(prompt))
-        except ValueError:
-            print("Please enter only floating numbers")
-            
-def get_string(prompt):
-    while True:
-        value = input(prompt).strip()
-        if value:
-            return value
-        print("Input cannot be empty.")
-
+    def get_float(self, prompt):
+        while True:
+            try:
+                return float(input(prompt))
+            except ValueError:
+                print("Please enter only floating numbers")
+                
+    def get_string(self, prompt):
+        while True:
+            value = input(prompt).strip()
+            if value:
+                return value
+            print("Input cannot be empty.")
 
 class ExpenseTracker():
     def __init__(self):
         self.expenses = []
-        
+        self.input = InputHelper()
+    
     def add_expense(self):
-        amount =  get_float("Enter amount: ")
-        category = get_string("Enter category: ")
-        note = get_string("Enter note: ")
+        
+        amount = self.input.get_float("Enter Amount: ")
+        category = self.input.get_string("Enter Category: ")
+        note = self.input.get_string("Enter Note: ")
         
         expense = {
-            "amount": amount,
-            "category": category,
-            "note": note 
+            "amount":amount,
+            "category":category,
+            "note":note
         }
         
         self.expenses.append(expense)
+        
+    def view_expenses(self):
+        
+        if not self.expenses:
+            print("No Expenses to Display!")
+            return
+        
+        print(f"\n ===All Expenses=== ")
+        for i, expense in enumerate(self.expenses, 1):
+            print(f"Expense {i}:")
+            print(f"Amount: {expense['amount']}")
+            print(f"Category: {expense['category']}")
+            print(f"Note: {expense['note']}")
+            
+    
+    def total_expense(self):
+        
+        if not self.expenses:
+            print("No Expenses to Display: ")
+            return
 
+        total = 0 
+    
+        for expense in self.expenses:
+            total += expense["amount"]
+            
+        print(f"Total Expenses are: {total}")
+        return total    
+        
+    def category_expense(self):
+        
+        if not self.expenses:
+            print("No Expenses to Display: ")
+            return 
+        
+        summary = {}
+        
+        for expense in self.expenses:
+            amount = expense["amount"]
+            category = expense["category"]
 
-
+            if category in summary:
+                summary[category] += amount 
+            else: 
+                summary[category] = amount
+            
+        print(f"\n---Category Summary---")
+        for category, amount in summary.items():
+            print(f"Category: {category}")
+            print(f"Amount: {amount}")
+        
+        return summary 
+    
+        
 # def add_expenses(expenses):
     
 #     num_of_expenses = get_int("Enter number of expense: ")
@@ -61,54 +114,55 @@ class ExpenseTracker():
         
 #     # return expenses
     
-def view_expenses(expenses):
-    if not expenses:
-        print("No Expenses to Display")
-        return 
-    print("\n----All Expenses----")
-    for i, expense in enumerate(expenses, 1):
-        print(f"Expense {i}:")
-        print(f"Amount: {expense['amount']}")
-        print(f"Category: {expense['categroy']}")
-        print(f"Note: {expense['note']}")
+# def view_expenses(expenses):
+#     if not expenses:
+#         print("No Expenses to Display")
+#         return 
+#     print("\n----All Expenses----")
+#     for i, expense in enumerate(expenses, 1):
+#         print(f"Expense {i}:")
+#         print(f"Amount: {expense['amount']}")
+#         print(f"Category: {expense['categroy']}")
+#         print(f"Note: {expense['note']}")
         
         
-def total_expenses(expenses):
-    if not expenses:
-        print("No expenses to Display")
-        return
+# def total_expenses(expenses):
+#     if not expenses:
+#         print("No expenses to Display")
+#         return
 
-    total = 0
-    for expense in expenses:
-        total += expense["amount"]
+#     total = 0
+#     for expense in expenses:
+#         total += expense["amount"]
 
-    print(f"Total Expenses are {total}")
-    return total
+#     print(f"Total Expenses are {total}")
+#     return total
 
-def category_expenses(expenses):
-    if not expenses:
-        print("No expenses to Display")
-        return
+# def category_expenses(expenses):
+#     if not expenses:
+#         print("No expenses to Display")
+#         return
     
-    summary = {}
+#     summary = {}
     
-    for expense in expenses:
-        amount = expense["amount"]
-        category = expense["category"]
+#     for expense in expenses:
+#         amount = expense["amount"]
+#         category = expense["category"]
         
-        if category in summary:
-            summary[category] += amount
-        else:
-            summary[category] = amount
+#         if category in summary:
+#             summary[category] += amount
+#         else:
+#             summary[category] = amount
              
-    print(f"\n----Category Summay----")
-    for category, total in summary.items():
-        print(f"{category}: {total}")
+#     print(f"\n----Category Summay----")
+#     for category, total in summary.items():
+#         print(f"{category}: {total}")
         
-    return summary
+#     return summary
 
 def menu():
-    expenses = []
+    tracker = ExpenseTracker()
+    input_helper = InputHelper()
 
     while True:
         print("\n===== Expense Tracker =====")
@@ -118,19 +172,19 @@ def menu():
         print("4. Category Summary")
         print("5. Exit")
 
-        choice = get_int("Enter your choice (1-5): ")
+        choice = input_helper.get_int("Enter your choice (1-5): ")
 
         if choice == 1:
-            add_expenses(expenses)
+            tracker.add_expense()
 
         elif choice == 2:
-            view_expenses(expenses)
+            tracker.view_expenses()
 
         elif choice == 3:
-            total_expenses(expenses)
+            tracker.total_expense()
 
         elif choice == 4:
-            category_expenses(expenses)
+            tracker.category_expense()
 
         elif choice == 5:
             print("Goodbye!")
@@ -142,4 +196,6 @@ def menu():
 
 if __name__ == "__main__":
     menu()
+
+
 
